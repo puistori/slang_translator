1abfc93d92121dd6d609d1152e61bac0315b8ed9
puistori <71samparker@gmail.com>
2019-08-05 19:14:18 -0700

    main resources for dealing with manyworded entries

---
+++ core_files/Finnish Slang/manyworded_stem_translator.py
@@ -0,0 +1,126 @@
+import pickle
+
+group1 = pickle.load(open('manyworded_entries/manyword_entries_g1','rb'))
+group1_second_members = pickle.load(open('manyworded_entries/manyword_entries_g1_second_pair_members','rb'))
+group1_verbs = pickle.load(open('manyworded_entries/manyword_entries_g1_verbs','rb'))
+
+group2 = pickle.load(open('manyworded_entries/manyword_entries_g2','rb'))
+#!!! this should be a dictionary.
+group2_last_words = pickle.load(open('manyworded_entries/manyword_entries_g2_last_words','rb'))
+
+group3 = pickle.load(open('manyworded_entries/manyword_entries_g3','rb'))
+group3_layer_2 = pickle.load(open('manyworded_entries/manyword_entries_g3_layer_2','rb'))
+
+group4 = pickle.load(open('manyworded_entries/manyword_entries_g4','rb'))
+#!!! this shoudl be a dictionary.
+group4_last_words = pickle.load(open('manyworded_entries/manyword_entries_g4_last_words','rb'))
+
+
+#!!! what if you have a manyworded instance separated by a comma? e.g. 'voi, ei!' - I could see this causing some problems with the highlighting function
+
+def manyworded_stem_translator(candidate,history,text_instance):
+    # First, we need to see if it is in group 1. First, let's see if the current word would be a second word in one of those pairs.
+    # I've assumed that the second word, which is the argument of the verb pair, will always have the same form. If I need to do
+    # lemmatization work with it, then I would need to slightly redesign this function. But it would be a doable adjustment - I would just
+    # do a set-overlap test with this first 'if' statement. 
+    if candidate in group1_second_members:
+        # what if some of the olla verbs allow for 'na' endings as well . . .?
+        # Ok. If it is, then we can see if the previous word is one of the group1_verbs:
+        if set(lemmatize_verb(history[len(history)-1])) & set(group1_verbs):
+            # It was! Let's grab their intersection.
+            manyworded_candidate = list( set(group1_verbs).intersection(set(lemmatize_verb(history[len(history)-1]))) )[0]
+            manyworded_candidate = manyworded_candidate + ' ' + candidate
+            # This should give us the manyworded stem. I assume that each of these expressions is composed only of two words.
+            # So we should see something like "puhua kreikaa" coming out of the woodwork here.
+            if manyworded_candidate in group1:
+                # add her up!
+                answers = group1[manyworded_candidate]
+                lemma = manyworded_candidate
+                instance = history[len(history)-1] + ' ' + candidate
+
+                return({(lemma,instance):answers})
+    # Now let's see if it's in group2 - left branching madness.
+    #!!! actually - watch out - you might be double-lemmatizing. 
+    if candidate in set(group2_last_words):
+        # I assume there will only be one match anyway. 
+        # Ok, so match is the final word of a group2 entry that matches up with our current word.
+        # (this line is not necessary in the current implementation, I'm effectively just renaming the variable.)
+        match =  candidate
+        # For every idiom that word is a part of, let's see if the history matches up with it. If it does, then we have a match.
+        for idiom in group2_last_words[match]:
+            good_match = True
+            for i in range(len(idiom)-1):
+                corresponding_word_in_history = history[len(history)-i]
+                split_idiom = idiom.split(' ')
+                # trim off the last word - we've already checked for that.
+                split_idiom = split_idiom[0:len(split_idiom)-1]
+                corresponding_word_in_idiom = split_idiom[len(split_idiom)-i]
+
+                if corresponding_word_in_idiom not in set(lemmatize_nominal(corresponding_word_in_history)):
+                    #uh-oh, looks like there was a mismatch :( 
+                    good_match = False
+                    break
+            if good_match:
+                # add her up!
+                lemma = idiom
+                instance = history[len(history)-(len(idiom)-1):].join(' ') + ' ' + text_instance
+                answers = group2[idiom]
+                return({(lemma,instance):answers})
+    
+    # Now let's see if it's in group 3. 
+    if candidate in group3:
+        # add her up!
+        entry = group3[candidate]
+        answers = group3_layer_2[entry]
+        lemma = entry
+        instance = text_instance
+        return({(lemma,instance):answers})
+        
+    # Now let's see if it's in group 4.
+
+    if candidate in group4_last_words:
+        for idiom in group4_last_words[candidate]:
+            good_match = True
+            for i in range(len(idiom.split(' '))-1):
+                print(i)
+                corresponding_word_in_history = history[ (len(history)-1)-i]
+                
+                split_idiom = idiom.split(' ')
+                # trim off the last word - we've already checked for that.
+                split_idiom = split_idiom[0:len(split_idiom)-1]
+                corresponding_word_in_idiom = split_idiom[(len(split_idiom)-1)-i]
+                print(" %s in histroy vs %s in idiom "%(corresponding_word_in_history,corresponding_word_in_idiom))
+
+                if corresponding_word_in_history != corresponding_word_in_idiom:
+                    good_match = False
+                    print('i bwoke')
+                    break
+
+            if good_match:
+                # add her up! the lemma and the instance should be the same for this kind of entry.
+                answers = group4[idiom]
+                return({(idiom,idiom):answers})
+
+    return None 
+
+
+## Now let's test this function
+
+# group 4 entry
+history_g4 =['haha','ok','nope','ladata','lyijyä']
+answer = manyworded_stem_translator('naamariin',history_g4,'naamariin')
+print('ok, here we go, g4:')
+print(answer)
+
+# group 3 entry
+answer = manyworded_stem_translator('bloukkaus',history_g4,'bloukkaudella')
+print(answer)
+
+# group 2 entry
+
+print('\n')
+# black out & bongi muna
+history_g2 = ['haha','ok','wtf','lol','black']
+#answer = manyworded_stem_translator('out',history_g2,'out')
+print(group2['black out'])
+print(group2_last_words['out'])
---
+++ manyworded_entry_grouper.py
@@ -0,0 +1,219 @@
+"""
+The purpose of this file is to divide the many-worded entries into different groups that can be handled. Many-worded entries are entries
+that are composed of more than one word, like "voi ei!". There are four groups that I've made:
+
+1. These are pairs beginning with a recognizable verb. "puhua kreikaa" is a good example.
+
+2. These are leftbranching noun phrases, like "poliittinen itsemurha"
+
+3. These are either/or entries, where the entry is meant to cover either one word or another, like "siftata / shiftata"
+
+4. These are whole idioms, like "voi ei!"
+
+"""
+
+
+import pickle
+import re
+
+sanalista = open('core_files/Finnish Slang/sanalista/kotus-sanalista_v1.xml','rt')
+
+normal_words = []
+
+for line in sanalista.readlines():
+    q = re.search(r'(?<=<s>).*?(?=</s>)',line)
+    # This is sloppy, I know. But the original file has 'out' as an entry in it for some reason. 
+    # I don't want that, but I also don't want to alter the original file.
+    if q and q.group(0) !='out':
+        normal_words.append(q.group(0))
+
+# Checking for membership is quicker in sets, so we'll use a set. 
+normal_words = set(normal_words)
+
+
+database = pickle.load(open('resources/slang_database','rb'))
+
+
+double_lemmas = {}
+
+other_count = 0
+count = 0
+
+group1_count = 0
+group2_count = 0
+group3_count = 0
+group4_count = 0
+
+# entries with just one word.
+one_word_entries = {}
+
+# Now, these dictionaries are full of entries that have more than one word.
+
+# Group 1 is composed of verb pairs where the first member is a conjugatable verb, and the second member is an argument of the verb.
+# For now, I am accepting only olla, saada, puhua, ottaa and soittaa ; this may be subject to change.
+group1 = {}
+group1_verbs = set(['olla','saada','puhua','ottaa','soittaa'])
+group1_second_pair_members = set([])
+# Group 2 is composed of left-branching nominal expressions. The last words maps the last word of an idiom to a list of idioms it's a part of.
+group2 = {}
+group2_last_words = {}
+# Group 3 is composed of multi-key entries, like ' shiftata / siftata' which points to a definition of 'either one'
+# This will be treated by putting in a 'second layer' - each individual word will be mapped to the compound lemma (e.g, 
+#  'siftata' by itself will map to 'shiftata / siftata' and then that compound will map to the meaning.)
+group3 = {}
+group3_second_layer = {}
+# Group 4 is composed of whole idioms and the like. A perfect match will be required. the last words maps the last word of an idiom to a list of idioms it's a part of.
+group4 = {}
+group4_last_words = {}
+
+
+
+
+for key in database:
+    # This first condition filters for all many-word entries. 
+    if " " in key:
+
+        if 'black out' in key:
+            print("yeah, it passed this way. what of it?")
+            print(key)
+
+        count += 1
+        found = False
+        # Now we need to figure out what sub-group they fall into.
+        stem = key.split(' ')[0]
+        # Filtering for words that start with a recognizable lemma. These words could fall into groups 1, 2, or 4.
+        if stem in normal_words:
+
+            if 'black out' in key:
+                print("yeah, it passed this way. part 1. what of it?")
+                print(key)
+
+            # It could belong to group 2, if the first word is an adjective
+            if stem in ['pieni','iso'] or stem.endswith('inen'):
+                group2_count += 1
+                
+                group2[key.replace('?','').replace('!','')] = database[key]
+                if key.split(' ')[len(key.split(' '))-1] in group2_last_words:
+                    group2_last_words[key.split(' ')[len(key.split(' '))-1]].append(key)
+                else:
+                    group2_last_words[key.split(' ')[len(key.split(' '))-1]] = [key]
+                found = True
+            # Group 1 is only taking specific verbs for the time being.
+            elif stem in group1_verbs:
+
+                group1_count += 1
+                
+                group1[key.replace('?','').replace('!','')] = database[key]
+                group1_second_pair_members.add(key.split(' ')[1])
+                found = True
+            else:
+                # In this case, we'll just punt it over to group 4. 
+                group4_count += 1
+                group4[key.replace('?','').replace('!','')] = database[key]
+                if key.split(' ')[len(key.split(' '))-1] in group4_last_words:
+                    group4_last_words[key.split(' ')[len(key.split(' '))-1]].append(key)
+                else:
+                    group4_last_words[key.split(' ')[len(key.split(' '))-1]] = [key]
+                found = True
+        # group 2 - searching for entries who end in a recognizable word.
+        if key.split(' ')[len(key.split(' '))-1] in normal_words and found == False:
+                
+            if 'black out' in key:
+                print("yeah, it passed this way. part 2. what of it?")
+                print(key)
+                print(key.split(' ')[len(key.split(' '))-1])
+            
+                for word in normal_words:
+                    if word == "out":
+                        print("well, yeah I guess")
+                        print(word)
+
+            
+                group2_count += 1
+                
+                group2[key.replace('?','').replace('!','')] = database[key]
+                if key.split(' ')[len(key.split(' '))-1] in group2_last_words:
+                    group2_last_words[key.split(' ')[len(key.split(' '))-1]].append(key)
+                else:
+                    group2_last_words[key.split(' ')[len(key.split(' '))-1]] = [key]
+                found = True
+        # group 3 - searching for entries containing a slash or a comma.
+        if ',' in key or '\/' in key:
+                group3_count += 1
+                
+                group3_second_layer[key] = database[key]
+                for ele in re.split('(,|\/)', key):
+                    group3[ele.replace('?','').replace('!','')] = key
+                found = True
+            
+        # group 4
+        if found == False:
+                group4_count += 1
+                if 'black out' in key:
+                    print("yeah, it passed this way into the second round. what of it?")
+                    print(key)
+                    print(key.split(' ')[len(key.split(' '))-1])
+                group4[key.replace('?','').replace('!','')] = database[key]
+                if key.split(' ')[len(key.split(' '))-1] in group4_last_words:
+                    group4_last_words[key.split(' ')[len(key.split(' '))-1]].append(key)
+                else:
+                    group4_last_words[key.split(' ')[len(key.split(' '))-1]] = [key]
+
+                found = True
+            
+    else:
+        other_count +=1
+
+print(count)
+print(other_count)
+#print("now by groups:")
+#print(group1,group2,group3,group4)
+
+
+g1 = open('manyword_entries_g1','wb')
+pickle.dump(group1,g1)
+g1.close()
+
+g1_v = open('manyword_entries_g1_verbs','wb')
+pickle.dump(group1_verbs,g1_v)
+g1_v.close()
+
+
+g1_second_pair_members = open('manyword_entries_g1_second_pair_members','wb')
+pickle.dump(group1_second_pair_members,g1_second_pair_members)
+g1_second_pair_members.close()
+
+
+g2 = open('manyword_entries_g2','wb')
+pickle.dump(group2,g2)
+g2.close()
+
+
+g2_last_words = open('manyword_entries_g2_last_words','wb')
+pickle.dump(group2_last_words,g2_last_words)
+g2_last_words.close()
+
+
+g3 = open('manyword_entries_g3','wb')
+pickle.dump(group3,g3)
+g3.close()
+
+g3_layer_2 = open('manyword_entries_g3_layer_2','wb')
+pickle.dump(group3_second_layer,g3_layer_2)
+g3_layer_2.close()
+
+g4 = open('manyword_entries_g4','wb')
+pickle.dump(group4,g4)
+g4.close()
+
+g4_last_words = open('manyword_entries_g4_last_words','wb')
+pickle.dump(group4_last_words,g4_last_words)
+g4_last_words.close()
+
+print("ok, done lol")
+
+# We're seeing two-worded verb-noun pairs, left-branching stemmer pairs (--> some of these have more than one word and should be put into group 4), manyformed entries ( , / )  and whole idioms,
+
+# for group one, common words are : olla, saada, puhua, soittaa , ottaaa
+# you probably want to clean the entries  - take away their exclamations and stuff.
+# 'no' should make it go to group 4 ; 'ei' should put it in group 4 (or 3), or 'voi' ,or 'se' . . . maybe look into conjugations with -inen , iso and pieni . . .
